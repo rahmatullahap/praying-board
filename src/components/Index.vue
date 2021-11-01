489276cc760dd55e97e334dee092e1b2ff76128b
Rahmatullah Arrizal <rahmatullaharrizal@gmail.com>
2021-11-02 02:23:04 +0700

    Adjust position and new API

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,18 +1,111 @@
 <template>
   <div>
-    <div class="mx-6">
-      <div class="flex justify-between">
-        <div class="flex flex-col items-center justify-center h-screen">
-          <div class="text-center my-4 text-3xl font-semibold">
-            <div>{{ date }}</div>
-            <div class="font-nunito">{{ time }}</div>
+    <div class="relative m-0">
+      <!-- <div class="flex flex-col items-center justify-center h-screen relative"> -->
+      <div class="h-screen">
+        <div
+          class="header w-full grid grid-cols-3 gaps-2 bg-white pt-4"
+          style="z-index: 2"
+        >
+          <div
+            class="
+              text-center
+              my-4
+              text-3xl
+              font-semibold
+              flex flex-row
+              items-center
+            "
+          >
+            <div class="font-nunito w-full text-3xl m-2">
+              <div>
+                {{ formatDate(date) }}
+              </div>
+            </div>
           </div>
-          <img class="logo" src="../assets/logo-alamanah.png" />
-          <div class="font-ubuntu text-center">
-            <h1 class="text-2xl font-extrabold my-2">Masjid Al Amanah</h1>
-            <h2 class="font-semibold">Jl. Kawaluyaan Indah I No.11</h2>
-            <div class="mt-2">{{ location.city }}</div>
+          <div>
+            <img class="logo" src="../assets/logo-alamanah.png" />
+            <div class="font-ubuntu text-center">
+              <h1 class="text-2xl font-extrabold my-2">Masjid Al Amanah</h1>
+              <h2 class="font-semibold">Jl. Kawaluyaan Indah I No.11</h2>
+            </div>
           </div>
+          <div
+            class="
+              text-center
+              my-4
+              text-3xl
+              font-semibold
+              flex flex-row
+              items-center
+            "
+          >
+            <div
+              class="
+                bg-blue-midnight
+                text-white
+                border border-solid
+                rounded-2xl
+                font-nunito
+                w-full
+                text-6xl
+                m-2
+              "
+            >
+              <div>{{ time }} WIB</div>
+            </div>
+          </div>
+        </div>
+        <div class="mt-4 mx-auto p-4 w-1/2">
+          <div
+            class="
+              main-content
+              text-center text-2xl
+              font-semibold
+              flex flex-row
+              items-center
+            "
+          >
+            <div
+              class="my-4 items-center h-full w-full"
+              v-if="beforeSholat < itv && beforeSholat > 0"
+            >
+              <div class="uppercase text-3xl font-semibold">Menuju Adzan</div>
+              <div
+                class="
+                  bg-blue-midnight
+                  text-white
+                  border border-solid
+                  rounded-2xl
+                  font-nunito
+                "
+              >
+                <span class="text-6xl">{{ formatTime(beforeSholat) }}</span>
+              </div>
+              <div class="text-2xl mt-2">
+                Rasulullah shallallahu ‘alaihi wa sallam bersabda, “Dan
+                seseorang dari kalian senantiasa dihitung dalam keadaan shalat
+                selama shalat itu menahannya (dia menanti palaksanaan shalat).
+                Di mana tidak ada yang menghalangi dia untuk kembali kepada
+                keluarganya kecuali shalat itu.” (HR. Bukhari no. 659 dan Muslim
+                no. 649)
+              </div>
+            </div>
+            <div v-else>
+              <VuePlayerVideo
+                src="https://kawaluyaan-rw05.com/amanah/assets/video2.mp4"
+                :autoplay="true"
+                :loop="true"
+                :controls="false"
+                :colors="'#1D2088'"
+              ></VuePlayerVideo>
+            </div>
+            <!-- <div class="text-center" v-if="iqomah < itv && iqomah > 0">
+              {{ formatTime(iqomah) }} Menuju Iqomah
+            </div> -->
+          </div>
+        </div>
+        <div class="flex flex-col items-center w-screen">
           <div class="flex items-center font-nunito mt-5">
             <Time
               :label="'Subuh'"
@@ -40,33 +133,14 @@
               :active="active === 'Isha'"
             ></Time>
           </div>
-          <div class="text-center my-4 text-2xl font-semibold">
-            <div
-              class="text-center"
-              v-if="beforeSholat < itv && beforeSholat > 0"
-            >
-              {{ formatTime(beforeSholat) }} Menuju Adzan
-            </div>
-            <div class="text-center" v-if="iqomah < itv && iqomah > 0">
-              {{ formatTime(iqomah) }} Menuju Iqomah
-            </div>
-          </div>
-        </div>
-        <div class="ml-2 w-1/2">
-          <div class="flex flex-col items-center justify-center h-screen">
-            <VuePlayerVideo
-              src="https://kawaluyaan-rw05.com/amanah/assets/construction.mp4"
-              :autoplay="true"
-              :loop="true"
-            ></VuePlayerVideo>
-          </div>
         </div>
       </div>
+      <div class="absolute bottom-4 w-full text-white bg-blue-midnight">
+        <marquee-text :repeat="10" class="text-2xl">
+          - Al Amanah Kawaluyaan Kota Bandung 
+        </marquee-text>
+      </div>
     </div>
-    <marquee-text>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna.
-    </marquee-text>
   </div>
 </template>
 
@@ -87,6 +161,65 @@ export default {
     MarqueeText,
   },
   methods: {
+    getPrayTime: function () {
+      // for testing
+      // this.pray = {
+      //   Imsak: "04:01",
+      //   Sunrise: "05:19",
+      //   Fajr: "04:11",
+      //   Dhuhr: "11:33",
+      //   Asr: "14:48",
+      //   Maghrib: "15:25",
+      //   Isha: "15:30",
+      // };
+      // const url =
+      //   "https://api.pray.zone/v2/times/today.json?school=3&juristic=1&longitude=106.8597248&latitude=-6.1702144&elevation=6";
+
+      const now = DateTime.local();
+
+      let day = now.get("day");
+      let dd = day.toString();
+      if (day < 10) {
+        dd = "0" + dd;
+      }
+
+      const url = `https://api.myquran.com/v1/sholat/jadwal/1219/${now.get(
+        "year"
+      )}/${now.get("month")}/${dd}`;
+
+      axios.get(url).then((response) => {
+        // const result = response.data.results;
+        // const datetime = result.datetime[0];
+        // this.pray = datetime.times;
+        // this.location = result.location;
+
+        const today = now.toJSDate();
+        this.date = today;
+
+        const result = response.data.data;
+        this.location = result.lokasi;
+        const j = result.jadwal;
+        this.pray = {
+          Imsak: j.imsak,
+          Sunrise: j.terbit,
+          Fajr: j.subuh,
+          Dhuhr: j.dzuhur,
+          Asr: j.ashar,
+          Sunset: "00:00",
+          Maghrib: j.maghrib,
+          Isha: j.isya,
+          Midnight: "23:59",
+        };
+      });
+    },
+    formatDate: function (date) {
+      const now = DateTime.local(new Date(date));
+      const formatdate = now.setLocale("id").toFormat("EEEE, dd MMMM yyyy");
+      if (formatdate.includes("Minggu")) {
+        return formatdate.replace("Minggu", "Ahad");
+      }
+      return formatdate;
+    },
     formatTime: function (seconds) {
       const minute = Math.floor(seconds / 60);
       const second = Math.floor(seconds % 60);
@@ -157,7 +290,6 @@ export default {
       iqomah: null,
       // 10 minute after adzan
       itv: 600,
-      video: "../assets/video.mp4",
       playing: true,
     };
   },
@@ -166,29 +298,8 @@ export default {
     clearInterval(this.interval);
   },
   mounted() {
-    // for testing
-    // this.pray = {
-    //   Imsak: "04:01",
-    //   Sunrise: "05:19",
-    //   Fajr: "04:11",
-    //   Dhuhr: "11:33",
-    //   Asr: "14:48",
-    //   Maghrib: "15:25",
-    //   Isha: "15:30",
-    // };
-    axios
-      .get("https://api.pray.zone/v2/times/today.json?city=bandung")
-      .then((response) => {
-        const result = response.data.results;
-        const datetime = result.datetime[0];
-        this.pray = datetime.times;
-        this.location = result.location;
-
-        const today = DateTime.local().setLocale("id").toFormat("DDDD");
-        this.date = today;
-      });
-
     this.active = "Fajr";
+    this.getPrayTime();
 
     // update the time every second
     this.interval = setInterval(() => {
@@ -204,8 +315,11 @@ export default {
 
 <style scoped>
 .logo {
-  width: 150px;
-  height: auto;
+  width: auto;
+  height: 100px;
   margin: 0 auto;
+}
+.main-content {
+  min-height: 40vh;
 }
 </style>
